dc4bf0b400dfa69812bad7b35343227231f237cd
Yakovnikitin <88880505+Yakovnikitin@users.noreply.github.com>
2021-08-13 12:50:21 +0300

    Update FirstVar.UE4.js

--- FirstVar.UE4.js
+++ FirstVar.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('FirstVar.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Yakovnikitin/Car_Configurator/4d3c9a886347154e08d53d4e3ad5190af1e4cace/FirstVar.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
