4d3c9a886347154e08d53d4e3ad5190af1e4cace
Yakovnikitin <88880505+Yakovnikitin@users.noreply.github.com>
2021-08-13 12:46:29 +0300

    Update FirstVar.data.js

--- FirstVar.data.js
+++ FirstVar.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/User/Desktop/Archi/Migrate/01/FirstVar/Binaries/HTML5/FirstVar.data';
-    var REMOTE_PACKAGE_BASE = 'FirstVar.data';
+    var PACKAGE_NAME = https://dl.dropboxusercontent.com/s/dp8w53e2lmoy56v/FirstVar.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/dp8w53e2lmoy56v/FirstVar.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
